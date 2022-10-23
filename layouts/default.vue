d2e0a72cbb9b59897ff4d81cfccf3117052dca0d
Bar-skiy <vbar7575@gmail.com>
2022-10-23 13:56:46 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+    <main>
+      <div class="container">
+        <Nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
