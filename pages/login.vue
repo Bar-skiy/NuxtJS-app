a36e12cb6749b2a38aa7394baf96638ae86323aa
Bar-skiy <vbar7575@gmail.com>
2022-10-27 00:02:24 +0300

    add error+login

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <h1>login</h1>
+    <div class="form-group">
+      <input type="text" class="form-control" />
+    </div>
+    <div class="form-group">
+      <input type="password" class="form-control" />
+    </div>
+    <button class="btn btn-primary">Login</button>
+    <nuxt-link class="link" to="/">Home</nuxt-link>
+  </form>
+</template>
+
+<script>
+export default {
+  layout: "empty",
+};
+</script>
+
+<style scoped>
+form {
+  width: 600px;
+}
+input {
+  margin-bottom: 15px;
+  margin-top: 15px;
+}
+.link {
+   margin-left: 480px; 
+}
+
+</style>
